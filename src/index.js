3e93d4371a52a310a14b83bbacba8be38a67b990
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-29 09:43:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
  
-const z = document.getElementById("new-task-description")
-const w = document.getElementById("submit")
 
-  var li = document.createElement("li");
-li.appendChild(z);
-w.addEventListener("click", () => {
-  document.getElementById("tasks").appendChild(li);
-});
-w.Event.preventDefault
 
