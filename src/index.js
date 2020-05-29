4184f1c99100daa7067f18f983033c68edde3349
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-29 10:24:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
- function newElement() {
 
- var li = document.createElement("li");
+
+document.addEventListener("DOMContentLoaded", () => {
+  var li = document.createElement("li");
 var z = document.getElementById("myInput").value
 var t = document.createTextNode(z);
 li.appendChild(t);
 
   document.getElementById("tasks").appendChild(li)
-}
-var w = document.getElementById("submit")
-w.preventDefault();
-
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
 });
