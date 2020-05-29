c00ca880afc8277fce1d99f11b751c7992c13d6a
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-29 09:34:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  event.preventDefault;
-});
+
 const z = document.getElementById("new-task-description")
-const w = document.getElementById("new-task-description")
 
   var li = document.createElement("li");
 li.appendChild(z);
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("tasks").appendChild(li);
+});
 
-document.getElementById("tasks").appendChild(li);
+
