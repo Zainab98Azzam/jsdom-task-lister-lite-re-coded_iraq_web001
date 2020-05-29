92eb35b7783c1a6d7e8ef8dc20ad6b1c10fad69a
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-29 10:19:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 var z = document.getElementById("myInput").value
 var w = document.getElementById("submit")
 var t = document.createTextNode(z);
- 
+ w.preventDefault();
 li.appendChild(t);
 
   document.getElementById("tasks").appendChild(li)
